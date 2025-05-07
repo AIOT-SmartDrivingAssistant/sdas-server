af3c3d5e461211bc3ed4745a3e1e48f96985d5e2
Nguyễn Thành Nhân <142470690+Fkiril@users.noreply.github.com>
2025-05-07 11:53:18 +0700

    hotfix: header_middleware

--- src/middlewares/header_middleware.py
+++ src/middlewares/header_middleware.py
@@ -10,4 +10,7 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response.headers["Content-Security-Policy"] = "default-src 'self'"
         allowed_origins = os.getenv("ALLOWED_ORIGINS", "*")
         response.headers["Access-Control-Allow-Origin"] = allowed_origins
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Allow-Methods"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
         return response
